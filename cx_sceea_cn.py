a3689d23149810ee241d1432a28f4d30250ba14d
ldm2060 <ldm2060@outlook.com>
2023-02-19 13:49:39 +0800

    Add files via upload

---
+++ cx_sceea_cn.py
@@ -0,0 +1,69 @@
+# encoding=utf-8
+import os
+from time import sleep 
+import ddddocr
+from PIL import Image
+from main import suofang, ksfz, start_browser, succeed, send, kksh, kname
+
+ocr = ddddocr.DdddOcr()
+
+issuccess = False
+
+def process(browser):
+    global issuccess
+    url = "https://cx.sceea.cn/html/YJCJ.htm"
+    browser.get(url)
+    browser.switch_to.window(browser.window_handles[0])
+    browser.find_element('id', 'ksh').send_keys(kksh)
+    browser.find_element('id', 'sfzh').send_keys(ksfz)
+    browser.find_element('id', 'input_ValidateCode').click()
+    sleep(1)
+    rpic = browser.find_element('id', 'ranImg')
+    loc = rpic.location
+    size = rpic.size
+    left = loc['x']*suofang
+    top = loc['y']*suofang
+    right = (loc['x'] + size['width'])*suofang
+    botom = (loc['y'] + size['height'])*suofang
+    val = (left, top, right, botom)
+    result = "1"
+    while(len(result) != 4):
+        browser.find_element('id', 'kbq').click()
+        sleep(1)
+        browser.save_screenshot('login.png')
+        login_pic = Image.open('login.png')
+        #通过上下左右的值，去截取验证码
+        yzm_pic = login_pic.crop(val)
+        login_pic.close()
+        yzm_pic.save('login.png')
+        login_pic = open('login.png', 'rb')
+        img_bytes = login_pic.read()
+        login_pic.close()
+        result = ocr.classification(img_bytes)
+    os.remove('login.png')
+    browser.find_element('id', 'input_ValidateCode').send_keys(result)
+    browser.find_elements_by_class_name("btn")[0].click()
+    sleep(1)
+    browser.close()
+    browser.switch_to.window(browser.window_handles[0])
+    if kname in browser.page_source:
+       succeed(browser,"省教育考试院")
+       issuccess = True
+ 
+def main():
+    num = 0
+    limit = 5000
+    browser = start_browser()
+    while True:
+        if issuccess:
+            break
+        num += 1
+        if num >= limit:
+            break
+        print("第"+str(num)+"次查询...")
+        process(browser)
+    if issuccess != True:
+        send("错误","查询成绩出错")
+        browser.quit()
+
+main()
---
+++ uestc_edu_cn.py
@@ -0,0 +1,69 @@
+# encoding=utf-8
+import platform, time, os
+from time import sleep 
+from selenium import webdriver
+import ddddocr, urllib
+from PIL import Image
+from main import suofang, ksfz, kpass, start_browser, succeed, send
+
+def process():
+    global limit
+    ocr = ddddocr.DdddOcr()
+    browser = start_browser()
+    url = "https://zsgl.uestc.edu.cn/ksxt/login.aspx"
+    try:
+        browser_curl = browser.current_url
+    except:
+        browser_curl = "1"
+    while(browser_curl != "https://zsgl.uestc.edu.cn/ksxt/Default.aspx"):
+        browser.get(url)
+        browser.find_element('id', 'txtPassWord').send_keys(kpass)
+        browser.find_element('id', 'txtLoginName').send_keys(ksfz)
+        rpic = browser.find_element('id', 'myCode')
+        loc = rpic.location
+        size = rpic.size
+        left = loc['x']*suofang
+        top = loc['y']*suofang
+        right = (loc['x'] + size['width'])*suofang
+        botom = (loc['y'] + size['height'])*suofang
+        val = (left, top, right, botom)
+        result = "1"
+        while(len(result) != 4):
+            browser.find_element_by_xpath("//a[contains(text(),'看不清')]").click()
+            sleep(1)
+            browser.save_screenshot('login1.png')
+            login_pic = Image.open('login1.png')
+            #通过上下左右的值，去截取验证码
+            yzm_pic = login_pic.crop(val)
+            login_pic.close()
+            yzm_pic.save('login1.png')
+            login_pic = open('login1.png', 'rb')
+            img_bytes = login_pic.read()
+            login_pic.close()
+            result = ocr.classification(img_bytes)
+        os.remove('login1.png')
+        browser.find_element('id', 'txtyzm').send_keys(result)
+        browser.find_element('id', 'btnLogin').click()
+        sleep(2)
+        try:
+            browser_curl = browser.current_url
+        except:
+            browser_curl = "1"      
+    while(True):
+        browser.switch_to.default_content()
+        browser.switch_to.frame("MenuFrame")
+        browser.find_element_by_partial_link_text('初试成绩查询').click()
+        browser.switch_to.default_content()
+        browser.switch_to.frame("PageFrame")
+        if "该功能暂时关闭" not in browser.page_source:
+            succeed(browser,"成电网站")
+            break
+    browser.quit()
+
+
+def main():
+    global limit
+    limit = 5000
+    process()
+
+main()
